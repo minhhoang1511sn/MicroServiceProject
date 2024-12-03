35cf0ed052fe7642911b3df865b9dd51ab6cd94e
minhhoang1511sn <80461876+minhhoang1511sn@users.noreply.github.com>
2024-12-03 13:30:30 +0700

    apachekafka

--- employeeService/src/main/java/com/josHoangDev/employeeService/command/controller/EmployeeCommandController.java
+++ employeeService/src/main/java/com/josHoangDev/employeeService/command/controller/EmployeeCommandController.java
@@ -1,21 +1,32 @@
 package com.josHoangDev.employeeService.command.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.josHoangDev.employeeService.command.command.CreateEmployeeCommand;
 import com.josHoangDev.employeeService.command.command.DeleteEmployeeCommand;
 import com.josHoangDev.employeeService.command.command.UpdateEmployeeCommand;
 import com.josHoangDev.employeeService.command.model.EmployeeRequestModel;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/employees")
+@EnableBinding(Source.class)
 public class EmployeeCommandController {
 
     @Autowired
     private CommandGateway commandGateway;
+
+    @Autowired
+    private MessageChannel output;
 
     @PostMapping
     public String addEmployee(@RequestBody EmployeeRequestModel model){
@@ -36,5 +47,17 @@ public class EmployeeCommandController {
         DeleteEmployeeCommand command = new DeleteEmployeeCommand(employeeId);
         commandGateway.sendAndWait(command);
         return "Employee deleted successfully";
+    }
+
+    @PostMapping("sendMessage")
+    public void sendMessage(@RequestBody String message){
+
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(message);
+            output.send(MessageBuilder.withPayload(json).build());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 }
