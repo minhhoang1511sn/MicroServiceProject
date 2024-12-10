a349e0936b3884b5de29368ab16216354b5a3dd0
minhhoang1511sn <80461876+minhhoang1511sn@users.noreply.github.com>
2024-12-10 17:41:37 +0700

    userservice

---
+++ userservice/src/main/java/com/josHoangDev/userservice/controller/USerController.java
@@ -0,0 +1,31 @@
+package com.josHoangDev.userservice.controller;
+
+import com.josHoangDev.userservice.data.User;
+import com.josHoangDev.userservice.model.UserDTO;
+import com.josHoangDev.userservice.service.USerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class USerController {
+    @Autowired
+    private USerService userService;
+
+
+    @GetMapping("/listUser")
+    public List<User> getAllUser() {
+        return userService.getAllUser();
+    }
+
+    @PostMapping("/addUser")
+    public User addUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+    @PostMapping("/login")
+    public UserDTO login(@RequestBody UserDTO dto) {
+        return userService.login(dto.getUsername(), dto.getPassword());
+    }
+}
